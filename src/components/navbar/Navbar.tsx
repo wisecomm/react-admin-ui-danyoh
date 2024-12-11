e93490f3df973fb9994654c5516e47c0f9f4c354
danyoh <danyoh#gmail.com>
2024-12-11 23:08:35 +0900

    1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Navbar.scss'
 
 function Navbar() {
     return (
-        <div className='nabvar'>
+        <div className='navbar'>
             <div className='logo'>
-                <img src='log.svg' alt='' />
+                <img src='/logo.svg' alt='' />
                 <span>lamadmin</span>
             </div>
             <div className='icons'>
@@ -12,8 +13,8 @@ function Navbar() {
                 <img src="/app.svg" alt="" className='icon'/>
                 <img src="/expand.svg" alt="" className='icon'/>
                 <div className='notification'>
-                    <img src="/notification.svg" alt="" />
-                    <span>1</span>
+                    <img src="/notifications.svg" alt="" />
+                    <span>2</span>
                 </div>
                 <div className='user'>
                     <img src='https://blogpfthumb-phinf.pstatic.net/data5/2005/2/19/95/%B0%A1%BF%C2%BB%FD%C0%CF_%BC%B1%B9%B0_small.jpg?type=w161' alt="" />
